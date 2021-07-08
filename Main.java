5a227529fa2fe5429a89cd2f546f64d2f7376bfd
huzaifahaider2561 <87112782+huzaifahaider2561@users.noreply.github.com>
2021-07-08 11:10:42 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main  {
+	 public static void main(String[] args) {
+	        CalculatorWindow mainWindow = new  CalculatorWindow();
+	        mainWindow.run();
+
+	}
+}
